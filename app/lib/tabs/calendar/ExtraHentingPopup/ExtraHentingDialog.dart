d2d56b4535c12307a37876208262d5439d4ef5a6
Kaja <knowit@knowits-MacBook-Pro-219340.local>
2020-06-25 12:30:03 +0200

    test reset

--- app/lib/tabs/calendar/ExtraHentingPopup/ExtraHentingDialog.dart
+++ app/lib/tabs/calendar/ExtraHentingPopup/ExtraHentingDialog.dart
@@ -188,7 +188,7 @@ class _ExtraHentingDialogState extends State<ExtraHentingDialog> {
       );
 
       AlertDialog alert = AlertDialog(
-        title: Text("Tidspunktene stemmer ikke overens rem,ove"),
+        title: Text("Tidspunktene stemmer ikke overens"),
         content: Text("Starttidspunkt må være før slutttidspunkt."),
         actions: [
           okButton,
