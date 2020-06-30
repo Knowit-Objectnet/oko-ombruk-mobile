3ea792856504c8a99ba0b6566bab4bb6fe445648
Magnus Tidemann <magnus.tidemann@knowit.no>
2020-06-30 13:16:06 +0200

    Cleaning

--- app/lib/LoadingScreen.dart
+++ app/lib/LoadingScreen.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatefulWidget {
-  @override
-  _LoadingScreenState createState() => _LoadingScreenState();
-}
-
-class _LoadingScreenState extends State<LoadingScreen> {
-  @override
-  void initState() {
-    // _authenticate();
-    super.initState();
-  }
-
+class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(child: Text('Loading...')));
   }
-
-  // Only for concept testing
-  /*void _authenticate() async {
-    Future.delayed(Duration(seconds: 0),
-        () => Navigator.of(context).pushReplacementNamed('/login'));
-  }*/
 }
