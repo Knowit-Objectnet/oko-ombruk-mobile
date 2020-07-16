f8a98937dcd29d0ae3b7f6e48d5a4b39b58d05e8
Magnus Tidemann <magnus.tidemann@knowit.no>
2020-07-16 13:13:41 +0200

    Removed todo

--- app/lib/repositories/CalendarRepository.dart
+++ app/lib/repositories/CalendarRepository.dart
@@ -5,8 +5,6 @@ import 'package:ombruk/models/CalendarEvent.dart';
 class CalendarRepository {
   final CalendarApiClient apiClient;
 
-  // TODO: Save calendarEvents here instead of passing it down
-
   CalendarRepository({@required this.apiClient}) : assert(apiClient != null);
 
   Future<List<CalendarEvent>> getEvents() async {
